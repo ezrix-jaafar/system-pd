b0cca99952448c068402fd065920d7aa85a77668
ezrix-jaafar <ezzeriq@gmail.com>
2023-10-12 16:09:11 +0800

    Digital Product

---
+++ app/Models/DigitalProducts.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DigitalProducts extends Model
+{
+    use HasFactory;
+}
