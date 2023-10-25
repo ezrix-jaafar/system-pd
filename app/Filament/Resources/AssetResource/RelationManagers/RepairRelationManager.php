b7b377281a6fcd9b6305b640cbecf1b11f1ab9ba
ezrix-jaafar <ezzeriq@gmail.com>
2023-10-25 09:45:23 +0800

    Update RepairRelationManager.php

--- app/Filament/Resources/AssetResource/RelationManagers/RepairRelationManager.php
+++ app/Filament/Resources/AssetResource/RelationManagers/RepairRelationManager.php
@@ -6,6 +6,10 @@ use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
+use Filament\Tables\Actions\ActionGroup;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\ViewAction;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -22,10 +26,24 @@ class RepairRelationManager extends RelationManager
 //                    ->required()
 //                    ->maxLength(255),
                 Forms\Components\TextInput::make('company')
-                    ->required(),
+                    ->required()
+                    ->columnSpan('full'),
                 Forms\Components\DatePicker::make('send_date')
                     ->required(),
-                
+                Forms\Components\DatePicker::make('pickup_date')
+                    ->required(),
+                Forms\Components\TextInput::make('repair_cost')
+                    ->required(),
+                Forms\Components\TextInput::make('send_by')
+                    ->required(),
+                Forms\Components\FileUpload::make('payment_receipt')
+                    ->image()
+                    ->openable()
+                    ->downloadable()
+                    ->required(),
+                Forms\Components\Textarea::make('note')
+                    ->nullable()
+                    ->rows('3'),
             ]);
     }
 
@@ -34,7 +52,11 @@ class RepairRelationManager extends RelationManager
         return $table
             ->recordTitleAttribute('id')
             ->columns([
-                Tables\Columns\TextColumn::make('id'),
+                Tables\Columns\TextColumn::make('company'),
+                Tables\Columns\TextColumn::make('send_date'),
+                Tables\Columns\TextColumn::make('pickup_date'),
+                Tables\Columns\TextColumn::make('repair_cost'),
+                Tables\Columns\TextColumn::make('send_by'),
             ])
             ->filters([
                 //
@@ -43,8 +65,11 @@ class RepairRelationManager extends RelationManager
                 Tables\Actions\CreateAction::make(),
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
+                ActionGroup::make([
+                    ViewAction::make(),
+                    EditAction::make(),
+                    DeleteAction::make(),
+                ]),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
