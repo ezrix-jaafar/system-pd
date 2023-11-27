48d3c433c1e5226915e1cd04585bf7a5b75426a0
Ezrix <ezrix.jaafar@gmail.com>
2023-11-27 16:39:40 +0800

    Add new phone owner and asset

--- app/Models/Phone.php
+++ app/Models/Phone.php
@@ -4,8 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Phone extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'brand',
+        'model',
+        'ram',
+        'storage',
+        'serial_number',
+        'purchase_date',
+        'purchase_receipt',
+        'warranty_expired',
+        'condition',
+        'notes',
+    ];
+
+    public function PhoneOwner(): HasMany
+    {
+        return $this->hasMany(PhoneOwner::class);
+    }
+
 }
+
+
